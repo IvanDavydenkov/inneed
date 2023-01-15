c014f652cdde554101cade898815852856750920
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-01-16 02:59:27 +0300

    features:web brif is working, but only web

--- scripts/program.js
+++ scripts/program.js
@@ -2,7 +2,7 @@
 let curseList = document.querySelector('.curse-list')
 function makeUp(web) {
     curseList.insertAdjacentHTML('afterbegin',
-        ` <li class="curse-list__item ${web.id}">
+        ` <li class="curse-list__item">
 <div class="curse-list__head">
     <h3 class="curse-list__curse-name">${web.title}</h3>
     <div class="curse-list__links">
@@ -12,8 +12,8 @@ function makeUp(web) {
         <a href="${web.syllabuses}" class="curse-list__link">
             <p class="curse-list__dcs ">Силабус</p>
         </a>
-        <a href="${web.brif}" class="curse-list__link">
-            <p class="curse-list__dcs ${web.id}">Бриф</p>
+        <a href="${web.brif}" class="curse-list__link ${web.id}">
+            <p class="curse-list__dcs">Бриф</p>
         </a>
         <a href="${web.notion}" class="curse-list__link">
             <p class="curse-list__dcs">Ноушен</p>
@@ -59,6 +59,48 @@ let programmArray = [
 for (let i = 0; i < programmArray.length; i++) {
     makeUp(programmArray[i])
 }
-webBlock = document.querySelectorAll('.web')
 
-webBlock.addEventListener('click', () => { })
+let webBrifLink = document.querySelector('.web')
+
+
+function toggleWebBrif() {
+    curseList.insertAdjacentHTML(`afterbegin`, `
+<div class="pop-up">
+<div class="pop-up__container">
+    <ul>
+        <li>15 -20 ч / неделю, 3 академа (30 дней), каникулы после каждого модуля, 720 ч</li>
+        <li>спринт 2 недели / диплом</li>
+        <li>Карьерный трек: есть Программа трудоустр-ва, акселерация</li>
+        <li>Онбординг (15 минут) + Фритрек (20 ч.)</li>
+        <li>фронтенд-разработчик</li>
+        <li>Базовый синтаксис HTML и CSS</li>
+        <li>Язык программирования JavaScript </li>
+        <li>Библиотека React и её экосистема</li>
+        <li>Серверный JavaScript в Node.js</li>
+        <li>Инструмент Webpack (соединяет JavaScript и CSS-файлы)</li>
+        <li>Работа с макетом, построение сложных сеток, адаптивная вёрстка</li>
+        <li>Серверный API для сайта (бэкенд)</li>
+        <li>Группировка элементов технологией Grid Layout</li>
+        <li>Nginx, Система контроля версий Git, Методология БЭМ</li>
+        <li>VScode, bash, Я.Облако,GitHub Actions, Express.js, mongoDB</li>
+        <li>Не учим в этом курсе: JAVA, Go, Php</li>
+    </ul>
+    <button class="pop-up__close-button">
+        <img src="./images/pop-up__form-close-button.svg" alt="Кнопка закрыть форму"
+            class="pop-up__close-icon" />
+    </button>
+</div>
+</div>
+`
+    )
+    let closeBrifButton = document.querySelector('.pop-up__close-button')
+    let popUp = document.querySelector('.pop-up')
+    function deletePopUp() {
+        popUp.remove()
+    }
+    closeBrifButton.addEventListener('click', deletePopUp)
+
+}
+
+webBrifLink.addEventListener("click", toggleWebBrif)
+
