75e1df90f5437664dce50df176a0d8411cc844b9
Ivan Davydenkov <krolik14121975@yandex.ru>
2023-01-17 14:39:10 +0300

    features:make adaptive and add  some  items in  db

--- scripts/dataPopUp.js
+++ scripts/dataPopUp.js
@@ -1,12 +1,19 @@
-let webBrifLink = document.querySelector('.web')
-let webBootcampBrifLink = document.querySelector('.web-bootcamp')
-let webPlusBrifLink = document.querySelector('.web-plus')
-let reactDevBrifLink = document.querySelector('.react-dev')
-let middleFrontendBrifLink = document.querySelector('.middle-frontend')
-
+let webBrifLink = document.querySelector(".web");
+let webBootcampBrifLink = document.querySelector(".web-bootcamp");
+let webPlusBrifLink = document.querySelector(".web-plus");
+let reactDevBrifLink = document.querySelector(".react-dev");
+let middleFrontendBrifLink = document.querySelector(".middle-frontend");
+let asyncPythonBrifLink = document.querySelector(".async-python");
+let cPlusPlusBrifLink = document.querySelector(".c-plus-plus");
+let javaDevBrifLink = document.querySelector(".java-dev");
+let qaBrifLink = document.querySelector(".qa");
+let qaPlusBrifLink = document.querySelector(".qa-plus");
+let qaBootcampBrifLink = document.querySelector(".qa-bootcamp");
 
 function toggleBrif(web) {
-    curseList.insertAdjacentHTML(`afterbegin`, `
+  curseList.insertAdjacentHTML(
+    `afterbegin`,
+    `
 <div class="pop-up">
 <div class="pop-up__container">
     <ul class="pop-up__list">
@@ -19,32 +26,34 @@ function toggleBrif(web) {
 </div >
 </div >
     `
-    )
-    let closeBrifButton = document.querySelector('.pop-up__close-button')
-    let popUp = document.querySelector('.pop-up')
-    let popUpList = curseList.querySelector('.pop-up__list')
-    let popUpContainer = document.querySelector('.pop-up__info-box')
-    //delete pop-up
-    function deletePopUp() {
-        popUp.remove()
-    }
-    closeBrifButton.addEventListener('click', deletePopUp)
-    //Requier links
-    for (let i = 0; i < web.requireLinks.length; i++) {
-        popUpContainer.insertAdjacentHTML('afterbegin',
-            `
+  );
+  let closeBrifButton = document.querySelector(".pop-up__close-button");
+  let popUp = document.querySelector(".pop-up");
+  let popUpList = curseList.querySelector(".pop-up__list");
+  let popUpContainer = document.querySelector(".pop-up__info-box");
+  //delete pop-up
+  function deletePopUp() {
+    popUp.remove();
+  }
+  closeBrifButton.addEventListener("click", deletePopUp);
+  //Requier links
+  for (let i = 0; i < web.requireLinks.length; i++) {
+    popUpContainer.insertAdjacentHTML(
+      "afterbegin",
+      `
         <a target="_blank" href="${web.requireLinks[i].link}"><p class="pop-up__info-box-item">${web.requireLinks[i].name}</p></a>
-        `)
-    }
-    //Main brif
-    for (let i = 0; i < web.popUp.length; i++) {
-        popUpList.insertAdjacentHTML('afterbegin',
-            `
+        `
+    );
+  }
+  //Main brif
+  for (let i = 0; i < web.popUp.length; i++) {
+    popUpList.insertAdjacentHTML(
+      "afterbegin",
+      `
             <li class="pop-up__list-item">${web.popUp[i]}</li>
-        `)
-    }
-
-
+        `
+    );
+  }
 }
 
 // webBrifLink.addEventListener("click", () => {
@@ -52,18 +61,39 @@ function toggleBrif(web) {
 //         toggleBrif(programmArray[i])
 //     }
 // })
-webBrifLink.addEventListener('click', () => {
-    toggleBrif(web)
-})
-webBootcampBrifLink.addEventListener('click', () => {
-    toggleBrif(webBootcamp)
-})
-webPlusBrifLink.addEventListener('click', () => {
-    toggleBrif(webPlus)
-})
-reactDevBrifLink.addEventListener('click', () => {
-    toggleBrif(reactDev)
-})
-middleFrontendBrifLink.addEventListener('click', () => {
-    toggleBrif(middleFrontend)
-})
+webBrifLink.addEventListener("click", () => {
+  toggleBrif(web);
+});
+webBootcampBrifLink.addEventListener("click", () => {
+  toggleBrif(webBootcamp);
+});
+webPlusBrifLink.addEventListener("click", () => {
+  toggleBrif(webPlus);
+});
+reactDevBrifLink.addEventListener("click", () => {
+  toggleBrif(reactDev);
+});
+middleFrontendBrifLink.addEventListener("click", () => {
+  toggleBrif(middleFrontend);
+});
+
+asyncPythonBrifLink.addEventListener("click", () => {
+  toggleBrif(AsyncPython);
+});
+
+cPlusPlusBrifLink.addEventListener("click", () => {
+  toggleBrif(cPlusPlus);
+});
+javaDevBrifLink.addEventListener("click", () => {
+  toggleBrif(javaDev);
+});
+
+qaBrifLink.addEventListener("click", () => {
+  toggleBrif(qa);
+});
+qaBootcampBrifLink.addEventListener("click", () => {
+  toggleBrif(qaBootcamp);
+});
+qaPlusBrifLink.addEventListener("click", () => {
+  toggleBrif(qaPlus);
+});
